267d28d3e7eb787c63ac974e2dff3175f5ff0c40
luoluoqixi <luoqixi@luoqixi.com>
2026-04-30 21:29:05 +0800

    fix: expo react-native version

--- examples-bun/craby-expo-test/src/app/_layout.tsx
+++ examples-bun/craby-expo-test/src/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
+import { CrabyModuleTest } from "craby_module_test";
+
 export default function RootLayout() {
+  console.log("CrabyModuleTest:add(1, 2) == ", CrabyModuleTest.add(1, 2));
+  console.log("CrabyModuleTest:divide(1, 2) == ", CrabyModuleTest.divide(1, 2));
+  console.log("CrabyModuleTest:multiply(1, 2) == ", CrabyModuleTest.multiply(1, 2));
+  console.log("CrabyModuleTest:subtract(1, 2) == ", CrabyModuleTest.subtract(1, 2));
   return <Stack screenOptions={{ headerShown: false }} />;
 }
